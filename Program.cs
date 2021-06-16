7e2076d51d741d56df67ec97cbe82a9d97f53927
kevin123-web <71745245+kevin123-web@users.noreply.github.com>
2021-06-16 14:41:05 -0500

    EjemploConsola1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("La Fecha de hoy es " + DateTime.Now);
+        }
+    }
+}
